eb797f0acab584c7eecd83f0beb1143ed91ce828
LucasNotaro <lucas.sn@live.com>
2024-11-19 11:47:08 -0300

    implmentação de cards de curso com data binding e comunicação input/output

--- src/app/card-cursos/card-cursos.component.ts
+++ src/app/card-cursos/card-cursos.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MaterialModule } from '../material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-cursos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MaterialModule],
   templateUrl: './card-cursos.component.html',
   styleUrl: './card-cursos.component.css'
 })
 export class CardCursosComponent {
+  @Input() titulo: string = '';
+  @Input() professor: string = '';
+  @Input() imagemCurso: string = '';
+  @Input() avaliacao: number = 0;
+  @Input() estrelas: string[] = [];
 
 }
--- src/app/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos-lista/cursos-lista.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MaterialModule } from '../material.module';
+import { CommonModule } from '@angular/common';
+import { CardCursosComponent } from "../card-cursos/card-cursos.component";
 
 @Component({
   selector: 'app-cursos-lista',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MaterialModule, CardCursosComponent],
   templateUrl: './cursos-lista.component.html',
   styleUrl: './cursos-lista.component.css'
 })
 export class CursosListaComponent {
+
+  cursos = [
+    {
+      titulo: 'Python para Iniciantes',
+      professor: 'Prof. Júlio Santos',
+      imagemCurso: 'assets/images/cursos/python.png',
+      avaliacao: 4.9,
+      estrelas: Array(4).fill('assets/images/home/estrela.png')  // Array para 5 estrelas
+    },
+    {
+      titulo: 'Desenvolvimento Web',
+      professor: 'Prof. Paulo Sérgio Coelho',
+      imagemCurso: 'assets/images/cursos/desenvolvimento-web.jpg',
+      avaliacao: 5.0,
+      estrelas: Array(5).fill('assets/images/home/estrela.png')
+    },
+    {
+      titulo: 'Desenvolvimento de Apps',
+      professor: 'Prof. Marcos Andrade',
+      imagemCurso: 'assets/images/cursos/desenvolvimento-apps.jpg',
+      avaliacao: 5.0,
+      estrelas: Array(5).fill('assets/images/home/estrela.png')
+    },
+    {
+      titulo: 'PHP',
+      professor: 'Prof. Simas S. Filho',
+      imagemCurso: 'assets/images/cursos/php.png',
+      avaliacao: 5.0,
+      estrelas: Array(5).fill('assets/images/home/estrela.png')
+    },
+    {
+      titulo: 'Usabilidade e UX',
+      professor: 'Prof. Sandra S. Lima',
+      imagemCurso: 'assets/images/cursos/usabilidade.jpg',
+      avaliacao: 5.0,
+      estrelas: Array(5).fill('assets/images/home/estrela.png')
+    },
+    {
+      titulo: 'React Native',
+      professor: 'Prof. Júlio Santos',
+      imagemCurso: 'assets/images/cursos/react.png',
+      avaliacao: 5.0,
+      estrelas: Array(4).fill('assets/images/home/estrela.png')
+    }
+  ];
 
 }
